dda9ec3f022d55d3f7194b38d5c254dde590e9b9
Jasper Stemerdink <1028657@hr.nl>
2025-06-30 21:38:48 +0200

    change file type

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../lib/supabase'
 import { User } from '@supabase/supabase-js';
 
 export default function Dashboard() {
