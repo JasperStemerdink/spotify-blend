8c57da2748c81d46059ec072b79b0e22b4f7d8e3
Jasper Stemerdink <1028657@hr.nl>
2025-07-01 20:17:40 +0200

    en wie heeft er nou gelijk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     const signIn = async () => {
         await supabase.auth.signInWithOAuth({
             provider: 'spotify',
+            options: {
+                scopes: 'user-top-read',
+            },
         });
     }
 
@@ -13,6 +16,7 @@ export default function Home() {
         <main>
             <h1>Spotify Blend</h1>
             <button onClick={signIn}>Login with Spotify</button>
+            <button onClick={() => supabase.auth.signOut()}>Log out</button>
         </main>
     )
 }
