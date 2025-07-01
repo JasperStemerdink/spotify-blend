6acad5c5c27e5481c990ebd44ea11abc18a8f3ce
Jasper Stemerdink <1028657@hr.nl>
2025-07-01 19:35:03 +0200

    update key

--- api/save-tracks.js
+++ api/save-tracks.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
     "https://mjtwikohjkfngnadmumr.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qdHdpa29oamtmbmduYWRtdW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMTgzNjUsImV4cCI6MjA2NjY5NDM2NX0.046y73toESI4IGAkR7V0dzL8L4JYGisdj3D0-JKXsxk" // Use the secret admin key here, not the anon key
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qdHdpa29oamtmbmduYWRtdW1yIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTExODM2NSwiZXhwIjoyMDY2Njk0MzY1fQ.q_Nl4CNPd_of2wz2NCigWUHqi1e9s1C4P_BFLpTdtu8"
 );
 
 module.exports = async function handler(req, res) {
